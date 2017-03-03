46f59cf7ef2f7c8a60e204baaa99adaa14f0f9b5
Christopher Smith <cts2bu@virginia.edu>
2017-03-03 15:54:20 +0000

    Changing direction a bit - shifting to Spacy instead of NLTK. The main script now pulls a text, tokenizes it, and determines the word dependencies in the sentence.

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
+# -*- coding: utf-8 -*-
 """
 Christopher Smith
 King's College London 2017
 """
 
-from nltk import sent_tokenize, word_tokenize, pos_tag
-import os
-
+import spacy
 
 # A helper method - read in a file at the specified filesystem location and return it as a text block for tokenization.
+# Makes the text unicode because Spacy needs it to be unicode.
 def ingest_text(file_url):
     f = open(file_url, 'r')
     text = f.read()
-    return text
-
-# Read in the text and tokenize it into sentences.
-madding_crowd_text = ingest_text('corpus/Far from the Madding Crowd.txt')
-sentences = sent_tokenize(madding_crowd_text)
+    return unicode(text, 'utf8')
 
 male_char_names = []
-fem_char_names = []
-
-# Begin to parse the text - break it into words.
-for sentence in sentences:
-    tokens = word_tokenize(sentence)
-    # If the sentence contains our target search item (Bathsheba), mark it.
-    # Try for pronouns?
-    if 'Bathsheba' in tokens:
-        tagged_tokens = pos_tag(tokens)
-        print tagged_tokens
-        print '---'
+female_char_names = []
+# Read in the text and tokenize it into sentences
+doc_root = 'corpus' # Change this to whatever folder holds your texts
+text = ingest_text('corpus/unicode/Far from the Madding Crowd.txt')
+en_nlp = spacy.load('en')
+doc = en_nlp(ingest_text('corpus/unicode/Far from the Madding Crowd.txt'))
+outfile = open('output/Far from the Madding Crowd  - Dependencies.txt', 'w')
+# Parse the text
+# For each sentence, tokenize the words and determine their dependencies (part of speech)
+for sentence in doc.sents:
+    print >> outfile, sentence
+    for word in sentence:
+        if word.dep_ != 'punct' and not word.is_space:
+            print >> outfile, word, " : ", word.dep_
+    print >> outfile, '---'
+outfile.close()
